68b31ecb6915dd6fe3ea7a3125330642d0a573b9
Sureka400 <surekar.aids2024@citchennai.net>
2025-10-05 09:46:27 +0530

    Delete reviewrouter.js

--- reviewrouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { protect } = require('../middleware/auth');
-const reviewController = require('../controllers/reviewController');
-
-router.post('/', protect, reviewController.addReview);
-router.get('/book/:bookId', reviewController.getReviewsForBook);
-router.delete('/:id', protect, reviewController.deleteReview);
-
-module.exports = router;
